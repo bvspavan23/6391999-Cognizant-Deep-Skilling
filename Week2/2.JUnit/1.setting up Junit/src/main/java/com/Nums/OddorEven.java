c8943e0318ec72b6bc2aa2eebb87e8702f07e367
B.V.S.Pavan <bvspavan234@gmail.com>
2025-06-26 09:53:26 +0530

    Added Junit

---
+++ Week2/2.JUnit/1.setting up Junit/src/main/java/com/Nums/OddorEven.java
@@ -0,0 +1,12 @@
+package com.Nums;
+
+public class OddorEven {
+	public String isNum(int num) {
+		if(num%2==0) {
+			return num+"is a Even Number";
+		}
+		else {
+			return num+"is a Odd Number";
+		}
+	}
+}
---
+++ Week2/2.JUnit/2.assertions in unit/JunitAssertions/src/test/java/com/Week2/AssertionsTest.java
@@ -0,0 +1,30 @@
+package com.Week2;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+	@Test
+	public void equalsmethod() {
+		assertEquals(5, 2+3);
+	}
+	
+	@Test
+	public void truemethod() {
+		assertTrue(5>3);
+	}
+	
+	@Test
+	public void falsemethod() {
+		assertFalse(5<3);
+	}
+	
+	@Test
+	public void nullmethod() {
+		assertNull(null);
+	}
+	
+	@Test
+	public void notnullmethod() {
+		assertNotNull(new Object());
+	}
+}
