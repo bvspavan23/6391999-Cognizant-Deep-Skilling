886ea627c1b23f2b16f636672388a5777d089edf
B.V.S.Pavan <bvspavan234@gmail.com>
2025-07-18 11:42:36 +0530

    Added week 5

---
+++ Week5/6391999/microservices/account/src/main/java/com/cognizant/account/AccountController.java
@@ -0,0 +1,17 @@
+package com.cognizant.account;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Account getAccountDetails(@PathVariable String number) {
+        return new Account(number, "savings",100000);
+    }
+
+    record Account(String number, String type, int balance) {}
+}
+
